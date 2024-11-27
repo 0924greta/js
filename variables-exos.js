55d5a648e6a1c5fc5ee47722507ab946ab339b8a
pilou <p-l.b@hotmail.com>
2024-11-27 16:07:25 +0100

    ended course2

---
+++ variables-exos.js
@@ -0,0 +1,29 @@
+const toto = "toto";
+const titi = "toto";
+const tutu = "toto";
+const tata = "toto";
+
+let itShouldBeACamelCaseName // camelCase
+let it_should_be_a_snake_case // snake_case
+let ItShouldBeAPascalCase // PascalCase
+let it-should - be - a - kebab -case // kebab-case pour l'HTML only
+
+/**
+   * -------------------------------------------------------
+   * 1 - Renommer les variables correctement
+   * -------------------------------------------------------
+*/
+
+const firstname = "Bruno";
+
+let brunoDescription = "Salut je m'appelle Bruno et voici ma description";
+
+const brunoJob = "Analyste financier au Yémen";
+
+let brunoAge = 23;
+
+let brunoCurrentLivingCity = "Bordeaux";
+
+const numberList = [1, 23, 52, 77];
+
+const theBestSecretAgendOfTimeFirstname = "le prénom du plus grand agent secret de tous les temps";
